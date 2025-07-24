7af9bd9f5832d04f6b25347b32d14519f08a8082
Janeider Rojas <jrojas@cobiscorp.com>
2025-07-24 15:24:39 -0500

    JRT - feat(Backend) Se crea la aplicacion de carrito de compras

---
+++ cart/ShoppingCart.py
@@ -0,0 +1,71 @@
+class ShoppingCart:
+    def __init__(self, request):
+        self.request = request
+        self.session = request.session
+        shopping_cart = self.session.get('shopping_cart')
+
+        if not shopping_cart:
+            self.shopping_cart = self.session['shopping_cart'] = {}
+        else:
+            self.shopping_cart = shopping_cart
+
+
+    def save_cart(self):
+        self.session['shopping_cart'] = self.shopping_cart
+        self.session.modified = True
+
+
+    def clear_cart(self):
+        self.session['shopping_cart'] = {}
+        self.session.modified = True
+        self.shopping_cart = {}
+
+
+    def add_product(self, product):
+        id_producto = str(product.cod_producto)
+
+        if id_producto not in self.shopping_cart.keys():
+            self.shopping_cart[id_producto] = {
+                'nombre': product.nombre,
+                'descripcion': product.descripcion,
+                'precio': product.precio,
+                'imagen': product.imagen.url,
+                'cantidad': 1
+            }
+        else:
+            self.shopping_cart[id_producto]['cantidad'] += 1
+            self.shopping_cart[id_producto]['precio'] = product.precio * self.shopping_cart[id_producto]['cantidad']
+
+        self.save_cart()
+
+
+    def remove_product(self, product):
+        id_producto = str(product.cod_producto)
+
+        if id_producto in self.shopping_cart.keys():
+            del self.shopping_cart[id_producto]
+            self.save_cart()
+
+
+    def subtract_product(self, product):
+        id_producto = str(product.cod_producto)
+
+        if id_producto in self.shopping_cart.keys():
+            self.shopping_cart[id_producto]['cantidad'] -= 1
+            self.shopping_cart[id_producto]['precio'] -= product.precio
+
+            if self.shopping_cart[id_producto]['cantidad'] <= 0:
+                self.remove_product(product)
+
+            self.save_cart()
+
+
+    def get_total_price(self):
+        total = 0
+        for price in self.shopping_cart.items():
+            total += price['precio']
+        return total
+
+
+    def __str__(self):
+        return f"ShoppingCart(items={self.shopping_cart})"
---
+++ cart/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+from core.models import Producto
+from .ShoppingCart import ShoppingCart
+
+# Create your views here.
+def cart_view(request):
+    cart = ShoppingCart(request)
+    return JsonResponse(cart.shopping_cart, safe=False)
+
+
+def add_to_cart(request, product_id):
+    cart = ShoppingCart(request)
+    product = Producto.objects.get(cod_producto=product_id)
+    cart.add_product(product)
+    return redirect('cart_view')
+
+
+def subtract_from_cart(request, product_id):
+    cart = ShoppingCart(request)
+    product = Producto.objects.get(cod_producto=product_id)
+    cart.subtract_product(product)
+    return redirect('cart_view')
+
+
+def remove_product_cart(request, product_id):
+    cart = ShoppingCart(request)
+    product = Producto.objects.get(cod_producto=product_id)
+    cart.remove_product(product)
+    return redirect('cart_view')
+
+
+def clear_cart(request):
+    cart = ShoppingCart(request)
+    cart.clear_cart()
+    return redirect('cart_view')
